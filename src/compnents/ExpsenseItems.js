5d7de2f8f223d3ce2536edc140f6a21d3161a075
Yashas-Coder <76394152+Yashas-Coder07@users.noreply.github.com>
2023-11-12 22:01:59 +0530

    Add files via upload

--- src/compnents/ExpsenseItems.js
+++ src/compnents/ExpsenseItems.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import "./ExpenseItems.css";
 import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
 
 function ExpenseItem(props) {
 
     return (
         <div className='expense-item'>
             <ExpenseDate date={props.date} />
-            <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <h3><div>{props.Location}</div></h3>
-                <div className='expense-item__price'> Rs {props.amount.toString()} </div>
-            </div>
+            <ExpenseDetails amount={props.amount} Location={props.Location} />
+
         </div>
     );
 }
