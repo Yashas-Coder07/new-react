cd5ff02926af6bae15b7107397734294dede8349
Yashas-Coder <76394152+Yashas-Coder07@users.noreply.github.com>
2023-11-13 15:46:36 +0530

    State inside the new Component

--- src/compnents/NewExpense/ExpenseForms.js
+++ src/compnents/NewExpense/ExpenseForms.js
@@ -1,29 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import './NewExpense.css'
 function ExpenseForm() {
+    const [inputTitle, setInputTitle] = useState('');
+    const [inputAmount, setInputAmount] = useState('');
+    const [inputDate, setInputDate] = useState('');
 
-    const ChangeHandler = (e) => {
-        console.log(e.target.value);
+    const titleChangeHandler = (e) => {
+        setInputTitle(e.target.value);
     };
+    const dateChangeHandler = (d) => {
+        setInputDate(d.target.value);
+    }
+    const amountChangeHandler = (a) => {
+        setInputAmount(a.target.value);
+    }
     return (
         <div className="new-expense">
             <form>
                 <div className="new-expense__controls">
                     <div className="new-expense__control" >
                         <label>Enter Title</label>
-                        <input type="text" onChange={ChangeHandler} />
+                        <input type="text" onChange={titleChangeHandler} />
 
                     </div>
 
                     <div className="new-expense__control">
                         <label >Enter Amount</label>
-                        <input type="number" onChange={ChangeHandler} />
+                        <input type="number" onChange={amountChangeHandler} />
 
                     </div>
 
                     <div className="new-expense__control">
                         <label>Enter Date </label>
-                        <input type="date" onChange={ChangeHandler} />
+                        <input type="date" onChange={dateChangeHandler} />
 
                     </div>
                     <br></br>
