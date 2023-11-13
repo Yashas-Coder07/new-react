f49d40d517c500b0024780c0a5da468122bad793
Yashas-Coder <76394152+Yashas-Coder07@users.noreply.github.com>
2023-11-13 11:02:31 +0530

    Add files via upload

--- src/compnents/Expenses/ExpenseItems.js
+++ src/compnents/Expenses/ExpenseItems.js
@@ -2,17 +2,24 @@ import React from 'react';
 import "./ExpenseItems.css";
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
-import Card from '../UI/Card';
+import Card from '../UI/Cards';
 
 function ExpenseItem(props) {
+
+    const deleteElement = () => {
+        console.log(props);
+    };
 
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <ExpenseDetails amount={props.amount} Location={props.Location} />
-
+            <button onClick={deleteElement}>Delete Expense</button>
         </Card>
     );
+
 }
+
+
 
 export default ExpenseItem;
