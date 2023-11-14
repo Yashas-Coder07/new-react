8012d95b2c5240a1467b3838b4ac67fbdf0b261b
Yashas-Coder <76394152+Yashas-Coder07@users.noreply.github.com>
2023-11-14 18:15:31 +0530

    Add files via upload

--- src/compnents/Expenses/Expenses.js
+++ src/compnents/Expenses/Expenses.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
-
+import ExpenseChart from './ExpenseChart'
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -24,7 +24,7 @@ const Expenses = (props) => {
                     selected={filteredYear}
                     onChangeFilter={filterChangeHandler}
                 />
-
+                <ExpenseChart expense={filteredExpenses} />
                 <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
