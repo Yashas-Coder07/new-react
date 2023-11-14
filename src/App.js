43fb574e460949810494d25744599a5d1937d43c
Yashas-Coder <76394152+Yashas-Coder07@users.noreply.github.com>
2023-11-14 11:41:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,34 +2,44 @@
 import './App.css';
 import ExpenseItem from './component/Expenses/ExpenseItems';
 import ExpenseForm from './component/NewExpenses/ExpenseForms';
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './component/UI/Cards';
-const App = () => {
-  const expense = [
-    {
-      title: "Toilet paper",
-      amount: 100,
-      date: new Date(2021, 3, 12),
+const Intial_Expenses = [
+  {
+    id: 'e1',
+    title: "Toilet paper",
+    amount: 100,
+    date: new Date(2021, 3, 12),
 
-    },
-    {
-      title: "TV",
-      amount: 10000,
-      date: new Date(2021, 3, 2)
-    },
-    {
-      title: "Rice",
-      amount: 450,
-      date: new Date(2021, 3, 12)
-    }
-  ]
+  },
+  {
+    id: 'e2',
+    title: "TV",
+    amount: 10000,
+    date: new Date(2021, 3, 2)
+  },
+  {
+    id: 'e3',
+    title: "Rice",
+    amount: 450,
+    date: new Date(2021, 3, 12)
+  }
+];
+
+
+const App = () => {
+  const [expense, setExpenses] = useState(Intial_Expenses);
 
   function saveData(data) {
-    const totalData = {
+    const newData = {
       ...data,
       id: Math.random().toString()
-    };
-    console.log(totalData);
+    }
+    setExpenses(prevExpenses => {
+      const totalData = [newData, ...prevExpenses];
+      return totalData;
+    });
+
   };
 
   return (
@@ -38,7 +48,7 @@ const App = () => {
 
       {expense.map(index => (
 
-        <ExpenseItem title={index.title} amount={index.amount} date={index.date} />
+        <ExpenseItem amount={index.amount} title={index.title} key={index.id} date={index.date} />
 
       ))}
 
